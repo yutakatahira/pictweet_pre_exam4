e16fc774daf0bf6b84c7281205f396ecfebcd7fe
katahira yuta <mgy.90.24.yu@gmail.com>
2020-01-29 13:49:44 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at" "DESC")
   end
 end
